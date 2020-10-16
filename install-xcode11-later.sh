6227b6164a1b2291edfa8f8c9439db8740ca2919
kang <louweikang@163.com>
2020-10-16 22:48:10 +0800

    更新脚本，支持往后加simruntime里添加

--- install-xcode11-later.sh
+++ install-xcode11-later.sh
@@ -5,7 +5,23 @@ if [ $EUID != 0 ]; then
     exit $?
 fi
 
-sudo cp 1/* /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Library/Developer/CoreSimulator/Profiles/Runtimes/iOS.simruntime/Contents/Resources/RuntimeRoot/usr/lib/
-sudo cp 2/* /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/lib/
-sudo cp 3/* /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk/usr/lib/
-sudo cp 4/* /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator.sdk/usr/lib/
+sudo cp iOS.simruntime/* /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Library/Developer/CoreSimulator/Profiles/Runtimes/iOS.simruntime/Contents/Resources/RuntimeRoot/usr/lib/
+sudo cp MacOSX.sdk/* /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/lib/
+sudo cp iPhoneOS.sdk/* /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk/usr/lib/
+sudo cp iPhoneSimulator.sdk/* /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator.sdk/usr/lib/
+
+RUNTIMES_DIR="/Library/Developer/CoreSimulator/Profiles/Runtimes"
+if [ -d "$RUNTIMES_DIR" ]; then
+
+for i in $(ls -l $RUNTIMES_DIR |awk '/^d/ {print $NF}');
+do
+
+dir="${RUNTIMES_DIR}/iOS ${i}/Contents/Resources/RuntimeRoot/usr/lib/"
+
+if [ -d "$dir" ]; then
+sudo cp iOS.simruntime/* "$dir"
+fi
+
+done
+
+fi
